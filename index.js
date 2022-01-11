161e311d19c73c84d68e2105405f2f6eae8ecca8
howsilence <statton0104@gmail.com>
2022-01-11 09:28:46 -0500

    commit tree 276dd785d134f5679bb5b23182484ebc6d471fac

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    let Hq = 42
+    if (someValue > 42){
+        return (someValue - 42)
+    } else {
+        return (42 - someValue)
+    }
+};
+function distanceFromHqInFeet(someValue){
+    let Hq = 42
+    if (someValue > 42){
+        return (someValue - 42) * 264
+    } else {
+        return (42 - someValue) * 264
+    }
+};
+function distanceTravelledInFeet(start, destination){
+    return (destination - start) * 264
+};
+function calculatesFarePrice(start, destination){
+    let poop = (destination - start) * 264
+    let shit = poop - 400
+    
+};
